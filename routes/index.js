dc887b94abf548b01c96464ccf23caa271b2385c
ckaddouh <ckaddouh@gmail.com>
2022-10-21 14:19:23 -0400

    adding css

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+const {requiresAuth} = require('express-openid-connect');
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   let username = "";
+//   if (req.oidc.isAuthenticated())
+//     username = req.oidc.user.name;
+//   res.render('index', { title: 'tuluble ' +  username
+//   , style: "index" });
+// });
+
+// router.get('/profile', requiresAuth(), (req, res) => {
+//   res.send(req.oidc.user);
+// })
+
+module.exports = router; 
