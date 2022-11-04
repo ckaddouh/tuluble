6c5fe1737892f327e0f2e3486441aac4bf84fb1b
Annie <annie.li.29122@gmail.com>
2022-11-04 14:09:52 -0400

    website up!

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const {requiresAuth} = require('express-openid-connect');
+// const {requiresAuth} = require('express-openid-connect');
+
+router.get('/', function(req, res, next) {
+    res.render('index', { title: ' Home Page ',
+                          style: 'index.css'});
+  });
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
