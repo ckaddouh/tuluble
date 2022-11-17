05760bc35f288f7afd8036ab4cf7850894c07929
ckaddouh <ckaddouh@gmail.com>
2022-11-17 13:47:53 -0500

    express working on home page

--- app1.js
+++ app1.js
@@ -7,6 +7,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
+  <a href="html/inventory.hbs">Inventory</a>
 });
 
 server.listen(port, hostname, () => {
--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,14 @@ router.get('/', function(req, res, next) {
   });
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   let username = "";
-//   if (req.oidc.isAuthenticated())
-//     username = req.oidc.user.name;
-//   res.render('index', { title: 'tuluble ' +  username
-//   , style: "index" });
-// });
+router.get('/', function(req, res, next) {
+  // let username = "";
+  // if (req.oidc.isAuthenticated())
+  //   username = req.oidc.user.name;
+  // res.render('index', { title: 'tuluble ' +  username
+  // , style: "index" });
+  res.render('index', {title: 'tuluble', style: "index"});
+});
 
 // router.get('/profile', requiresAuth(), (req, res) => {
 //   res.send(req.oidc.user);
