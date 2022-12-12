b2b4687433291a5fb140e12a9a3c1f24e2c5989b
Christina Kaddouh <ChristinaKaddouh@gmail.com>
2022-12-12 14:11:28 -0500

    ingredient database fixed

--- db/select_projects.sql
+++ db/select_projects.sql
@@ -1,16 +1,4 @@
-SELECT 
-	project.project_id as project_id, 
-    project.project_name,
-    project.client,
-    DATE_FORMAT(date, '%Y-%m-%d') as date
-FROM 
-    project
--- 	project LEFT JOIN (
--- 		SELECT COUNT(*) as project_likes, project_id 
---         FROM likes
--- 		GROUP BY project_id -- likes.project_id
--- 	) as project_like_counts ON project.project_id = project_like_counts.project_id,
---     team, project_type
--- WHERE
---     project.project_team_id = team.team_id
---     and project.project_type_id = project_type.project_type_id
+SELECT
+    project_name, project_id, client, date
+FROM
+    projects
