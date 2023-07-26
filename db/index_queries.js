55e51b4043459c7bdf17312774d3721e97942585
Christina Kaddouh <ChristinaKaddouh@gmail.com>
2023-07-26 16:12:13 -0400

    index updates

--- db/index_queries.js
+++ db/index_queries.js
@@ -1,16 +1,6 @@
 const db = require("./db_connection");
 
 
-const getLowAmountsQuery = `
-  SELECT
-    ingredient_id, inci_name, trade_name, amt, expiration
-  FROM 
-    ingredient
-  WHERE
-    ingredient.low = 1 
-    AND ingredient.active = 1
-`
-
 const getExpiredQuery = `
   SELECT
     ingredient_id, inci_name, trade_name, amt, expiration
@@ -21,15 +11,6 @@ const getExpiredQuery = `
     AND ingredient.active = 1
 `
 
-const read_inventory_search = `
-  SELECT
-    ingredient_id, trade_name, classifier_id, lot_num, shelf, inci_name, amt, expiration, date_received, supplier, coa, msds, cost
-  FROM
-    ingredient
-  WHERE 
-    ingredient.inci_name LIKE ? AND ingredient.active = 1
-`
-
 const checkAdminQuery = `
   SELECT 
     admin 
@@ -38,9 +19,6 @@ const checkAdminQuery = `
   WHERE email = ?
 `
 
-function getLowAmounts(callback) {
-  db.execute(getLowAmountsQuery, callback);
-}
 
 function getExpired(callback) {
   db.execute(getExpiredQuery, callback);
@@ -50,4 +28,4 @@ function requireAdmin(email, callback) {
   db.execute(checkAdminQuery, [email], callback);
 }
 
-module.exports = { getLowAmounts, getExpired, requireAdmin};
+module.exports = { getExpired, requireAdmin};
