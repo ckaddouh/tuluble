623e94b28532866576c52d9af559e33e014fc523
Christina Kaddouh <ChristinaKaddouh@gmail.com>
2023-08-11 15:58:04 -0400

    index updates

--- db/index_queries.js
+++ db/index_queries.js
@@ -18,16 +18,6 @@ const checkAdminQuery = `
     scientist 
   WHERE email = ?
 `
-const readIndexClassifierSqlQuery = `
-SELECT
-  ingredient_id, inci_name, trade_name, amt, expiration
-FROM 
-  ingredient
-WHERE
-  ingredient.expiration <= UTC_DATE()
-  AND ingredient.active = 1
-  AND classifier_id = ?
-`
 
 
 function getExpired(callback) {
@@ -38,8 +28,5 @@ function requireAdmin(email, callback) {
   db.execute(checkAdminQuery, [email], callback);
 }
 
-function readIndexClassifierSql(classifier, callback) {
-  db.execute(readIndexClassifierSqlQuery, [classifier], callback);
-}
 
-module.exports = { getExpired, requireAdmin, readIndexClassifierSql};
+module.exports = { getExpired, requireAdmin };
