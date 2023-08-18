eee590962ec764cc33f994e1827e3d9ea91e664a
Christina Kaddouh <ChristinaKaddouh@gmail.com>
2023-08-18 09:45:20 -0400

    fixing login

--- routes/index.js
+++ routes/index.js
@@ -17,31 +17,56 @@ const authConfig = {
 router.use(auth(authConfig));
 
 router.get('/', async function (req, res, next) {
-  let error;
-  
-  const admin = await new Promise((resolve, reject) => {
-    db.requireAdmin(req.oidc.user.email, (error, admin) => {
-      if (error) reject (error);
-      else resolve(admin);
-    });
-  });
+  res.locals.isAuthenticated = req.oidc.isAuthenticated();
 
-  if (admin[0].admin === 0 || admin[0].admin === 1) {
-    if (error) {
-      res.redirect('/error'); // Internal Server Error
-    } else {
-      db.getExpired((error, expired) => {
+  if (res.locals.isAuthenticated) {
+    //check if admin
+
+    const exists = await new Promise((resolve, reject) => {
+      db.requireAdmin(req.oidc.user.email, (error, exists) => {
+        if (error) reject (error);
+        else resolve(exists);
+      });
+    });
+    
+
+    console.log("EXISTS");
+    console.log(exists);
+
+    if (exists.length > 0) {
+      const admin = await new Promise((resolve, reject) => {
+        db.requireAdmin(req.oidc.user.email, (error, admin) => {
+          if (error) reject (error);
+          else resolve(admin);
+        });
+      });
+    
+      if (admin[0].admin === 0 || admin[0].admin === 1) {
         if (error) {
           res.redirect('/error'); // Internal Server Error
         } else {
-          res.render('index', {expired, profileInfo: req.oidc.user.nickname, isAdmin: admin[0].admin});
+          db.getExpired((error, expired) => {
+            if (error) {
+              res.redirect('/error'); // Internal Server Error
+            } else {
+              res.render('index', {expired, profileInfo: req.oidc.user.nickname, isAdmin: admin[0].admin});
+            }
+          });
         }
-      });
+      }
+      else {
+        res.redirect("/inventory");
+      }
+  
+    } else {
+      res.redirect("/error")
     }
+
   }
-  else {
-    res.redirect("/inventory");
-  }
+  
+  let error;
+  
+  
 });
 
 
